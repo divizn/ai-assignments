c84a71d94680ec6ced0e42009c22c5bd79756b2e
divizn <hassanjaved186@gmail.com>
2023-10-18 11:32:42 +0100

    Finish clustering

--- Clustering/main.R
+++ Clustering/main.R
@@ -1,8 +1,9 @@
+# import WK function
 source("./WK_R.r")
-seed = read.csv('seeds_dataset.csv', sep = ',')
-plot(seed)
+
+# import datasets
+seed = read.csv('seeds_dataset.csv', sep = ',') 
 realseed = read.csv('seeds_real.csv', sep = ',')
-plot(realseed)
 
 
 # optional: Prepare Data
@@ -10,44 +11,115 @@ seed = na.omit(seed) # deletion of missing data
 seed = scale(seed) # standardize variables
 
 
-# Hierarchical Clustering
+#---------------------------------------#
+
+
+# Hierarchical Clustering - Single
+
+d <- dist(seed, method = "euclidean") # generate distance matrix between datapoints
+
+fit <- hclust(d, method="single") # perform clustering (s, a, c)
+plot(fit, main="Single Cluster Dendogram") # display dendogram
+
+Single_Hgroups <- cutree(fit, k=5) # cut tree into 5 clusters
+
+rect.hclust(fit, k=5, border="red") # draw the dendrogram with red borders around these 5 clusters
+plot(seed, col=Single_Hgroups, main="Single Linkage Method") # draw a scatterplot with the assigned clusters as colours
+
+
+# Hierarchical Clustering - Average
 
 d <- dist(seed, method = "euclidean") # generate distance matrix between datapoints
 
 fit <- hclust(d, method="average") # perform clustering (s, a, c)
-plot(fit) # display dendogram
+plot(fit, main="Average Cluster Dendogram") # display dendogram
 
-Hgroups <- cutree(fit, k=5) # cut tree into 5 clusters
+Average_Hgroups <- cutree(fit, k=5) # cut tree into 5 clusters
 
-rect.hclust(fit, k=5, border="red") # draw the dendrogram with red borders around these 5 clusters
-plot(seed, col=Hgroups) # draw a scatterplot with the assigned clusters as colours
+rect.hclust(fit, k=5, border="green") # draw the dendrogram with red borders around these 5 clusters
+plot(seed, col=Average_Hgroups, main="Average Linkage Method") # draw a scatterplot with the assigned clusters as colours
+
+
+# Hierarchical Clustering - complete
+
+d <- dist(seed, method = "euclidean") # generate distance matrix between datapoints
+
+fit <- hclust(d, method="complete") # perform clustering (s, a, c)
+plot(fit, main="Complete Cluster Dendogram") # display dendogram
+
+Complete_Hgroups <- cutree(fit, k=5) # cut tree into 5 clusters
+
+rect.hclust(fit, k=5, border="blue") # draw the dendrogram with red borders around these 5 clusters
+plot(seed, col=Complete_Hgroups, main="Complete Linkage Method") # draw a scatterplot with the assigned clusters as colours
+
+
+#---------------------------------------#
 
 
 # K Means Clustering
 
-fit <- kmeans(seed, 3) # 3 clusters solution (k=3)
+k=3
 
-aggregate(seed,by=list(fit$cluster),FUN=mean) # calculate mean value for each variable 
+fit <- kmeans(seed, k) # k clusters solution
 
-Kgroups = fit$cluster # split cluster assignments
+# aggregate(seed,by=list(fit$cluster),FUN=mean) # calculate mean value for each variable 
 
-plot(seed, col=Kgroups)
+Kgroups_3 = fit$cluster # split cluster assignments
 
+plot(seed, col=Kgroups_3, main="K-Means scatterplot for 3 clusters")
 
+#--
 
-# calculate weighted kappa for k means compared to hierarchical (and also correct clusters)
+k=4
 
+fit <- kmeans(seed, k) # k clusters solution
 
-wk = WK_R(Kgroups, Hgroups) # weighted kappa of KC vs HC
+# aggregate(seed,by=list(fit$cluster),FUN=mean) # calculate mean value for each variable 
 
+Kgroups_4 = fit$cluster # split cluster assignments
+
+plot(seed, col=Kgroups_4, main="K-Means scatterplot for 4 clusters")
+
+#--
+
+k=5
+
+fit <- kmeans(seed, k) # k clusters solution
+
+# aggregate(seed,by=list(fit$cluster),FUN=mean) # calculate mean value for each variable 
+
+Kgroups_5 = fit$cluster # split cluster assignments
+
+plot(seed, col=Kgroups_5, main="K-Means scatterplot for 5 clusters")
+
+#---------------------------------------#
+
+# get clusters from solution dataset to compare with KMeans and HC using weighted kappa
 fitreal_k <- kmeans(realseed, 3) 
-realGroup = fitreal_k$cluster
+real_group = fitreal_k$cluster
 
 
 # get kappa of other linkage methods and also random values of k for k means
 
+WK_KC3 = WK_R(Kgroups_3, real_group) # K Means where k=3 compared to actual cluster (weighted kappa value)
+WK_KC4 = WK_R(Kgroups_4, real_group) # K Means where k=4 compared to actual cluster (weighted kappa value)
+WK_KC5 = WK_R(Kgroups_5, real_group) # K Means where k=5 compared to actual cluster (weighted kappa value)
 
-wk1 = WK_R(Kgroups, realGroup) # K Means compared to actual cluster (weighted kappa value)
+WK_singleHC = WK_R(Single_Hgroups, real_group) # Hierarchical single linkage compared to actual cluster (weighted kappa value)
+WK_averageHC = WK_R(Average_Hgroups, real_group) # Hierarchical average linkage compared to actual cluster (weighted kappa value)
+WK_completeHC = WK_R(Complete_Hgroups, real_group) # Hierarchical complete linkage compared to actual cluster (weighted kappa value)
 
-wk2 = WK_R(Hgroups, realGroup) # Hierarchical compared to actual cluster (weighted kappa value)
 
+
+graph_x <- c("k=3","k=4","k=5","single","average","complete")
+graph_y <- c(WK_KC3, WK_KC4, WK_KC5, WK_singleHC, WK_averageHC, WK_completeHC)
+
+# barchart with added parameters
+barplot(graph_y,
+        main = "Weighted Kappa of clusters compared to actual clusters",
+        xlab = "Method/Algorithm",
+        ylab = "Weighted Kappa",
+        names.arg = graph_x,
+        col = "grey",
+        ylim=c(0,1)
+        )
