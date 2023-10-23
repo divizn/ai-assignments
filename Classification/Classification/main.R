316c575a5020fec0c9486104b7c8ff622f8c442f
divizn <hassanjaved186@gmail.com>
2023-10-23 16:34:29 +0100

    Update classification (hopefully finished)

--- Classification/Classification/main.R
+++ Classification/Classification/main.R
@@ -1,7 +1,12 @@
 # read data into R
 seeds <- read.csv('./seeds_dataset_class.csv', sep=",")
+# import/install libraries
+# install.packages("rpart")
+# install.packages("class")
+library(rpart)
+library(class)
 # split data into class and values variables
-seeds_rand <- seeds[sample(150, 150),]
+seeds_rand <- seeds[sample(150, 150),] # randomise data since data could be small or biassed so good to use resampling method
 seedsclass <- seeds_rand[,1]
 seedsvalues <- seeds_rand[,-1]
 
@@ -13,11 +18,7 @@ seedsclass_test <- seedsclass[100:150]
 seedsvalues_test <- seedsvalues[100:150,]
 
 
-# build decision tree using rpart
-# install.packages("rpart")
-library(rpart)
-
-# create decision tree and pruned decision trees (to compare)
+# create decision tree
 fit <- rpart(seedsclass_train~ ., data=seedsvalues_train, method="class")
 # plot decision tree
 plot(fit, uniform=TRUE, main="Decision tree for seeds data")
@@ -35,10 +36,48 @@ print(accuracy)
 table_mat <- table(seedsclass_test, pred)
 print(table_mat)
 
-# pruning data
-pfit <- prune(fit, cp=0.1)
-plot(pfit, uniform=TRUE, main="Pruned decision tree for seeds data")
-text(pfit, use.n=TRUE, all=TRUE, cex=.8)
 
 
-# q1 why - sample of data could be small or biassed so good to use resampling method
+# 2. Test it by scoring the accuracy on test data using different degrees of pruning using
+# prune.
+
+# testing classification by pruning the tree and comparing accuracy
+arr = array(dim=c(1,5)) # initialise array of size 1x5 (to fit pruned accuracy values)
+# want to test multiple pruned trees, so use for loop to get different values of cp (e.g. 0.1, 0.2, 0.3, etc.)
+for (i in 1:5) {
+  pfit<-prune(fit, cp=i/10)
+  plot(pfit, uniform = TRUE, main = "Pruned Decision Tree for SeedsData3 (cp = i/10") # display pruned tree where cp = i/10
+  text(pfit, use.n = TRUE, cex=.8)
+  p_treepred<-predict(pfit, seedsvalues_test, type = 'class')
+  p_n = length(seedsclass_test) # test case
+  p_ncorrect = sum(p_treepred==seedsclass_test) # correctly predicted
+  p_accuracy = p_ncorrect/p_n
+  arr[1,i]=p_accuracy # add accuracy value to array
+}
+print(arr) # print accuracies of pruned trees
+
+
+# 3. Scatterplot 2 selected variables of the data (and colour code according to the decision
+# tree output), display the learnt tree and calculate the accuracy.
+plot(seeds$Length, seeds$Width, col=pred)
+# (accuracy and tree displayed in previous code block)
+
+
+# 4. Compare the accuracy of the different pruned trees to KNN with different values of k
+arr = array(dim=c(2,5)) # 2x5 array to store accuracy values for pruned tree and knn
+for (i in 1:5) {
+  pfit<-prune(fit, cp=i/10)
+  plot(pfit, uniform = TRUE, main = "Pruned Decision Tree for SeedsData3")
+  text(pfit, use.n = TRUE, cex=.8)
+  p_treepred<-predict(pfit, seedsvalues_test, type = 'class')
+  p_n = length(seedsclass_test) #num of test case
+  p_ncorrect = sum(p_treepred==seedsclass_test) #num correctly predicted
+  p_accuracy = p_ncorrect/p_n
+  arr[1,i]=p_accuracy
+  knn3pred = knn(seedsvalues_train, seedsvalues_test, seedsclass_train, k=i) # generate predicted class
+  n_knn = length(seedsclass_test) # num test cases
+  ncorrect_knn = sum(knn3pred==seedsclass_test) #num correctly predicted
+  accuracy_knn = ncorrect_knn/n_knn
+  arr[2,i]=accuracy_knn
+}
+print(arr) # (1 is pruned tree, 2 is knn)
