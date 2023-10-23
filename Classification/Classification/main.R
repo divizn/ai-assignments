6809b00cfdf67674680f4667dbd3ee54e7dd6e6b
divizn <hassanjaved186@gmail.com>
2023-10-23 15:11:06 +0100

    Update classification (done q1)

--- Classification/Classification/main.R
+++ Classification/Classification/main.R
@@ -1,28 +1,44 @@
 # read data into R
-winedata <- read.csv('./winedata.csv', sep=",")
-
+seeds <- read.csv('./seeds_dataset_class.csv', sep=",")
 # split data into class and values variables
-wineclass <- winedata[,1]
-winevalues <- winedata[,-1]
+seeds_rand <- seeds[sample(150, 150),]
+seedsclass <- seeds_rand[,1]
+seedsvalues <- seeds_rand[,-1]
 
-# set up training and test datasets
-wineclass_train <- wineclass[1:100]
-winevalues_train <- winevalues[1:100,]
+# set up training and test datasets (holdout method)
+seedsclass_train <- seedsclass[1:100]
+seedsvalues_train <- seedsvalues[1:100,]
 
-wineclass_test <- wineclass[100:178]
-winevalues_test <- winevalues[100:178,]
+seedsclass_test <- seedsclass[100:150]
+seedsvalues_test <- seedsvalues[100:150,]
 
 
 # build decision tree using rpart
-install.packages("rpart")
+# install.packages("rpart")
 library(rpart)
 
-fit <- rpart(wineclass_train~ ., data=winevalues_train, method="class")
-
+# create decision tree and pruned decision trees (to compare)
+fit <- rpart(seedsclass_train~ ., data=seedsvalues_train, method="class")
 # plot decision tree
-plot(fit, uniform=TRUE, main="Decision tree for winedata")
+plot(fit, uniform=TRUE, main="Decision tree for seeds data")
 text(fit, use.n=TRUE, all=TRUE, cex=.8)
 
+# sensitivity analysis - test classification accuracy on test dataset by comparing predicted and actual values
+pred <- predict(fit, seedsvalues_test, type="class")
+
+n <- length(seedsclass_test) # num of test cases
+ncorrect <- sum(pred == seedsclass_test) # num of correct predictions
+accuracy <- ncorrect/n # accuracy of predictions
+print(accuracy)
+
+# show results in a confusion matrix table
+table_mat <- table(seedsclass_test, pred)
+print(table_mat)
+
+# pruning data
+pfit <- prune(fit, cp=0.1)
+plot(pfit, uniform=TRUE, main="Pruned decision tree for seeds data")
+text(pfit, use.n=TRUE, all=TRUE, cex=.8)
 
 
 # q1 why - sample of data could be small or biassed so good to use resampling method
